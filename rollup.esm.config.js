401c42d0f2b611a922de5e119b69361f2013f1aa
Manuel Valls Fernández <manolo@vlrz.es>
2018-09-02 00:54:03 +0200

    Switch to run-p, add esm output

---
+++ rollup.esm.config.js
@@ -0,0 +1,18 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/wit.esm.js',
+    format: 'esm',
+  },
+  plugins: [
+    babel({
+      babelrc: false,
+      exclude: 'node_modules/**',
+      presets: [
+        ['@babel/env']
+      ],
+    }),
+  ],
+};
